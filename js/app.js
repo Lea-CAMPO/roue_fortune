87218d0d7198a09d7750b9ecc34ed9db3cb92467
Léa Campo <campo.lea@gmail.com>
2021-12-16 11:35:07 +0100

    calcul part roue, reset rotation à chaque clic

--- js/app.js
+++ js/app.js
@@ -1,48 +1,73 @@
+/***************************** */
+/* Initialisation des variables */
+/***************************** */
+
+/* Compteur, roue et display */
 let compteur = 0;
-const display = document.querySelector('.display');
-  
-function spinTheWheel() {
+const display = document.querySelector(".display");
+const wheel = document.getElementById("wheel-face");
+const zoneSize = 30;
 
-    let arc = 360 / 6;
+/* Tableau des segments de la roue */
 
-    /* Génération de l'angle de la roue (valeur aléatoire) */
-    let deg = Math.floor(5000 + Math.random() *5000);
-    let actualDeg = deg %360;
-    let zoneSize= 45;
-   
-    /* Définition de l'animation de la roue */
-
-    const wheel = document.getElementsByClassName("roue");
-    let spinAnimation = gsap.to(wheel, {duration: 5, rotation: deg, ease:"bounce"});
-    spinAnimation.pause();
-
-    let symbolSegments = {
-        1:"Today's deal",
-        2:"Free samples",
-        3:"100$",
-        4:"Bag ???",
-        5:"Amazon coupon",
-        6:"Gift card",
-    };
-
-    const handleWin = (actualDeg) => {
-        const winningSymbolNr = Math.ceil(actualDeg / zoneSize);
-        display.innerHTML = symbolSegments[winningSymbolNr];
-      }
-
-      handleWin(actualDeg);
-
-    if (compteur < 3) {
-        
-        spinAnimation.play(0);
-        compteur ++;
-    }
-
-    else {
-        alert("Vous ne pouvez plus jouer")
-    }
-}
+let symbolSegments = [
+  "Bag ???",
+  "Today's Deal",
+  "Today's Deal",
+  "Free samples",
+  "Free samples",
+  "100$",
+  "100$",
+  "Amazing coupon",
+  "Amazing coupon",
+  "Gift card",
+  "Gift card",
+  "Bag ???",
+];
 
 const buttonSpin = document.getElementById("button-spin");
+buttonSpin.addEventListener("click", spinTheWheel);
 
-buttonSpin.addEventListener('click', spinTheWheel);
+/***************************** */
+/* Fonctions */
+/*************/
+
+/* Fonction de calcul de l'angle pour récupérer le segment du haut */
+
+function handleWin(actualDeg) {
+  const winningSymbolNr = Math.floor(actualDeg / zoneSize);
+  console.log(winningSymbolNr);
+  display.innerHTML = symbolSegments[winningSymbolNr];
+}
+
+/* Fonction d'animation de la roue déclenchée au clic */
+
+function spinTheWheel() {
+  /* Génération de l'angle de la roue (valeur aléatoire) */
+  let deg = Math.floor(5000 + Math.random() * 3600);
+  console.log("deg: ", deg);
+  let actualDeg = deg % 360;
+
+  handleWin(actualDeg);
+  console.log(wheel);
+  let spinAnimation = gsap.to(wheel, {
+    duration: 0,
+    rotation: 0,
+  });
+  spinAnimation.play(0);
+
+  if (compteur < 3) {
+    //setTimeout(() => {
+    /* Définition de l'animation de la roue */
+    let spinAnimation = gsap.to(wheel, {
+      duration: 5,
+      rotation: deg,
+      ease: "ease",
+    });
+    spinAnimation.play(0);
+    compteur++;
+    //});
+  } else {
+    alert("Vous ne pouvez plus jouer");
+  }
+}
